9cdee96a7f4ba7a59468e734c0954df3b83fe461
Aaron Parker <aaron@stealthpuppy.com>
2023-03-11 23:28:57 +1100

    Add proactive remediations

---
+++ Locale/Detect-SystemLocale.ps1
@@ -0,0 +1,81 @@
+<#
+    .SYNOPSIS
+        Get locale settings for the system.
+        Use with Proactive Remediations or PowerShell scripts
+
+    .NOTES
+ 	    NAME: Detect-SystemLocale.ps1
+	    VERSION: 1.0
+	    AUTHOR: Aaron Parker
+	    TWITTER: @stealthpuppy
+
+    .LINK
+        http://stealthpuppy.com
+#>
+[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification = "Output required by Proactive Remediations.")]
+param (
+    [System.String] $Locale = "en-AU"
+)
+
+# Select the locale
+switch ($Locale) {
+    "en-US" {
+        # United States
+        $GeoId = 244
+        $Timezone = "Pacific Standard Time"
+    }
+    "en-GB" {
+        # Great Britain
+        $GeoId = 242
+        $Timezone = "GMT Standard Time"
+    }
+    "en-AU" {
+        # Australia
+        $GeoId = 12
+        $Timezone = "AUS Eastern Standard Time"
+    }
+    default {
+        # Australia
+        $GeoId = 12
+        $Timezone = "AUS Eastern Standard Time"
+    }
+}
+
+# Test regional settings
+try {
+    # Get regional settings
+    Import-Module -Name "International"
+
+    # System locale
+    if ($Null -eq (Get-WinSystemLocale | Where-Object { $_.Name -eq $Locale })) {
+        Write-Host "System locale does not match $Locale."
+        Exit 1
+    }
+
+    # Language list
+    if ($Null -eq (Get-WinUserLanguageList | Where-Object { $_.LanguageTag -eq $Locale })) {
+        Write-Host "Language list does not match $Locale."
+        Exit 1
+    }
+
+    # Home location
+    if ($Null -eq (Get-WinHomeLocation | Where-Object { $_.GeoId -eq $GeoId })) {
+        Write-Host "Home location does not match $Locale."
+        Exit 1
+    }
+
+    # Time zone
+    if ($Null -eq (Get-TimeZone | Where-Object { $_.Id -eq $Timezone })) {
+        Write-Host "Time zone does not match $Timezone."
+        Exit 1
+    }
+
+    # All settings are good exit cleanly
+    Write-Host "All regional settings match $Locale and $Timezone."
+    exit 0
+}
+catch {
+    Write-Host $_.Exception.Message
+    exit 1
+}
---
+++ Shortcuts-Public/Detect-PublicDesktopShortcuts.ps1
@@ -0,0 +1,65 @@
+<#
+    .SYNOPSIS
+        Removes shortcuts from the Public desktop.
+        Use with Proactive Remediations or PowerShell scripts
+
+    .NOTES
+ 	    NAME: Detect-PublicDesktopShortcuts.ps1
+	    VERSION: 1.0
+	    AUTHOR: Aaron Parker
+	    TWITTER: @stealthpuppy
+
+    .LINK
+        http://stealthpuppy.com
+#>
+[CmdletBinding()]
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSAvoidUsingWriteHost", "", Justification = "Output required by Proactive Remediations.")]
+param ()
+
+#region Functions
+function Get-KnownFolderPath {
+    <#
+        .SYNOPSIS
+            Gets a known folder's path using GetFolderPath.
+        .PARAMETER KnownFolder
+            The known folder whose path to get. Validates set to ensure only known folders are passed.
+        .NOTES
+            https://stackoverflow.com/questions/16658015/how-can-i-use-powershell-to-call-shgetknownfolderpath
+    #>
+    param (
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('AdminTools', 'ApplicationData', 'CDBurning', 'CommonAdminTools', 'CommonApplicationData', 'CommonDesktopDirectory', 'CommonDocuments', 'CommonMusic', `
+                'CommonOemLinks', 'CommonPictures', 'CommonProgramFiles', 'CommonProgramFilesX86', 'CommonPrograms', 'CommonStartMenu', 'CommonStartup', 'CommonTemplates', `
+                'CommonVideos', 'Cookies', 'Desktop', 'DesktopDirectory', 'Favorites', 'Fonts', 'History', 'InternetCache', 'LocalApplicationData', 'LocalizedResources', 'MyComputer', `
+                'MyDocuments', 'MyMusic', 'MyPictures', 'MyVideos', 'NetworkShortcuts', 'Personal', 'PrinterShortcuts', 'ProgramFiles', 'ProgramFilesX86', 'Programs', 'Recent', `
+                'Resources', 'SendTo', 'StartMenu', 'Startup', 'System', 'SystemX86', 'Templates', 'UserProfile', 'Windows')]
+        [System.String] $KnownFolder
+    )
+    [Environment]::GetFolderPath($KnownFolder)
+}
+#endregion
+
+# Get shortcuts from the Public desktop
+try {
+    $PublicDesktop = Get-KnownFolderPath -KnownFolder "CommonDesktopDirectory"
+    $FileTypes = "*.lnk"
+    $Shortcuts = Get-ChildItem -Path $PublicDesktop -Filter $FileTypes
+}
+catch {
+    Write-Host "Failed when enumerating shortcuts at: $PublicDesktop. $($_.Exception.Message)"
+    exit 1
+}
+
+# If $Shortcuts > 1
+# Output all shortcuts in a list with line breaks in a single output
+if ($Shortcuts.Count -ge 1) {
+    foreach ($Shortcut in $Shortcuts) {
+        $Output += "$($Shortcut.FullName)`n"
+    }
+    Write-Host "Found shortcuts:`n$Output"
+    exit 1
+}
+
+# All settings are good exit cleanly
+Write-Host "No shortcuts found at: $PublicDesktop."
+exit 0
