48218f24f843dd7a19d550867c4093d03cfae860
Aaron Parker <aaron@stealthpuppy.com>
2023-03-12 16:39:36 +1100

    Update script collection

--- tests/Remediate.Tests.ps1
+++ tests/Remediate.Tests.ps1
@@ -10,10 +10,9 @@ param ()
 BeforeDiscovery {
     # TestCases are splatted to the script so we need hashtables
     $Scripts = Get-ChildItem -Path "$env:GITHUB_WORKSPACE" -Recurse -Include "Remediate*.ps1"
-    $TestCases = $Scripts | ForEach-Object { @{file = $_ } }
 }
 
-Describe -Tag "Detect" -Name "<Script.Name> runs OK" -ForEach $TestCases {
+Describe -Tag "Detect" -Name "<Script.Name> runs OK" -ForEach $Scripts {
     BeforeAll {
         # Renaming the automatic $_ variable to $application to make it easier to work with
         $Script = $_
