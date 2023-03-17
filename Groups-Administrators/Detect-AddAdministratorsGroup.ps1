1eaa5bab3244483f2a2696564eb395ceb659622e
Aaron Parker <aaron@stealthpuppy.com>
2023-03-17 21:46:07 +1100

    Add Remediate-AddAdministratorsGroup

---
+++ Groups-Administrators/Detect-AddAdministratorsGroup.ps1
@@ -0,0 +1,20 @@
+<#
+    Determine whether the primary user of the device is in the Administrators group
+    ! Need to find a way to turn domain\username into UPN for detection and match against the primary user
+#>
+[CmdletBinding()]
+param()
+
+function Get-EnrollmentsRegistryKey {
+    try {
+        $EnrollmentsKey = "HKLM:\SOFTWARE\Microsoft\Enrollments\"
+        $MatchingKey = foreach ($Key in (Get-ChildItem -Path $EnrollmentsKey | Where-Object { $_.PSIsContainer -eq $true })) {
+            $Key | Where-Object { ($_.Property -match "ProviderID") -and ($_.Property -match "UPN") -and ($_.Property -match "AADTenantID") }
+        }
+    }
+    catch {
+        $MatchingKey = $Null
+        throw "Failed to return key."
+    }
+    Write-Output -InputObject $MatchingKey
+}
