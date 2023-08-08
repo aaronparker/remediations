3dc210ee5ea08b21b00bf16bfe30de5d57ea053e
Aaron Parker <aaron@stealthpuppy.com>
2023-08-08 20:43:50 +1000

    Get-DeviceHealthScripts

---
+++ Groups-Administrators/Get-LocalAdministrators.ps1
@@ -0,0 +1,13 @@
+#use the old net localgroup command to get members of the local Administrators group,
+# then parse the text output, excluding the built-in Administrator user and Domain Admins group
+$LocalAdmins = net localgroup administrators | `
+    Where-Object { $_ -and $_ -notmatch "command completed successfully" -and $_ -notmatch '^Administrator$' -and $_ -notmatch 'Domain Admins' } | `
+    Select-Object -Skip 4
+
+#Convert the text to an array
+$LocalAdmins = $LocalAdmins.Split([Environment]::NewLine) | ? { $_ -ne "" }
+
+#Example of looping through the array
+foreach ($user in $LocalAdmins) {
+    Write-Host "$user is a local admin"
+}
